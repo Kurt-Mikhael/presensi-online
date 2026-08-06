e6295a178b5f9aed5d79c9f86cb83f7e19b49cec
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-08-06 13:38:44 +0700

    fix: force https application urls in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureRateLimiting();
         Carbon::setLocale(config('app.locale', 'id'));
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
